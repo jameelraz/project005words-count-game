e0e47463949144c219dc462cbf1e80b2c7271772
jameel raza <166707907+jameelraz@users.noreply.github.com>
2024-05-12 10:01:46 -0700

    word counter game..
    a cli base intresting word count game.

---
+++ index.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.bold('welcome to my word counter game!'));
+let condition = true;
+while (condition) {
+    const answers = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: chalk.yellow.bold("enter your sentence to count the words"),
+        },
+        {
+            name: "continue",
+            message: "do you want to continue or Quit",
+            type: "confirm",
+            default: "false",
+        }
+    ]);
+    let words = answers.sentence.trim().split(" ");
+    condition = (answers.continue);
+    console.log(words);
+    console.log(chalk.red.bold(`your sentence word count is ${words.length}`));
+}
